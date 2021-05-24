8e9d70c84a334402ef3aa7e8e28999fe0ba9ab77
Ruth Gregory <ruthannrgregory@gmail.com>
2021-05-24 16:46:21 -0700

    Update cda.py
    Make polynomial fits for each ramp up

--- cda.py
+++ cda.py
@@ -15,19 +15,29 @@
  *
  * =======================================================================================
  """
+import matplotlib.pyplot as plt
+import numpy as np
 
 print("Some message about what this script is")
 print()
 
 #Get file path and name from user
-file_path = input("Enter the full path and name of your input data file: ")
+#file_path = input("Enter the full path and name of your input data file: ")
+file_path = '/Users/ruthgregory/Documents/SRF/Data/before_bake/QWR_217MHz_cooldown_10uT_2019_12_06_08h30_QoData.txt'
 file = open(file_path, "r")
 
 #Get information about the input data table from user
-Qcol_str = input("Enter the column number of your Quality Factor data: ")
-Tcol_str = input("Enter the column number of your temperature data: ")
-G_str = input("Enter the Geometric factor: ")
-skiplines_str = input("Optional input: Enter the number of lines to skip at the beginning of the table, or to not skip any lines, press enter: ")
+#Qcol_str = input("Enter the column number of your Quality Factor data: ")
+#Tcol_str = input("Enter the column number of your temperature data: ")
+#Eacccol_str = input("Enter the column number of your accelerating field data: ")
+#G_str = input("Enter the Geometric factor: ")
+#skiplines_str = input("Optional input: Enter the number of lines to skip at the beginning of the table, or to not skip any lines, press enter: ")
+
+Qcol_str = '11'
+Tcol_str = '24'
+Eacccol_str = '9'
+G_str = '120'
+skiplines_str = '4'
 
 skiplines = 0 #The default number of lines to skip at the beginning of the table is zero
 
@@ -37,16 +47,20 @@ if skiplines_str != "":
 
 Qcol = int(Qcol_str)
 Tcol = int(Tcol_str)
+Eacccol = int(Eacccol_str)
 G = int(G_str)
 
 #Subtract 1 because Python indexing starts at 0
 Qcol -= 1
 Tcol -= 1
+Eacccol -= 1
 
 #Create empty lists to hold the data
 Qdata = []
-Rsdata = []
 Tdata = []
+Eaccdata = []
+Rssdata = [] #This will be the non-corrected Rs* data: G/Qo
+
 
 #Read the input file line by line and extract data
 line_cnt = 1 #line count, it will start with line 1
@@ -87,11 +101,59 @@ for line in file:
         line_cnt += 1
         continue
 
+    try:
+        Eaccdata.append(float(columns[Eacccol]))
+    except IndexError:
+        print("Warning: index error on line", line_cnt, "in the Eacc column. Skipping line", line_cnt)
+        Qdata.pop()
+        Tdata.pop()
+        line_cnt += 1
+        continue
+    except ValueError:
+        print("Value on line", line_cnt, "in the Eacc column is not a number. Skipping line", line_cnt)
+        Qdata.pop()
+        Tdata.pop()
+        line_cnt += 1
+        continue
+
     line_cnt += 1
 
 file.close()
 
+#Get the non-corrected Rs* data
 for Q in Qdata:
-    Rsdata.append(G/Q)
+    Rssdata.append(G/Q)
 
-print(len(Tdata), len(Rsdata))
+
+ramp_Eacc = []
+ramp_Rss = []
+
+for i in range(0,len(Eaccdata)):
+
+    if i == 0:
+        ramp_Eacc.append(Eaccdata[i])
+        ramp_Rss.append(Rssdata[i])
+        continue
+
+    if Eaccdata[i-1] < Eaccdata[i]:
+        ramp_Eacc.append(Eaccdata[i])
+        ramp_Rss.append(Rssdata[i])
+    else:
+
+        #Do the fit here
+        coef = np.polyfit(ramp_Eacc,ramp_Rss,3) #3rd degree polynomial
+        #print(coef)
+        fit = np.poly1d(coef)
+        #print(fit)
+
+        #Get ready to start the next ramp up fit
+        ramp_Eacc.clear()
+        ramp_Rss.clear()
+        ramp_Eacc.append(Eaccdata[i])
+        ramp_Rss.append(Rssdata[i])
+
+print(ramp_Eacc)
+
+print(len(Tdata), len(Rssdata), len(Eaccdata))
+#plt.plot(Tdata, Rsdata)
+#plt.show()
